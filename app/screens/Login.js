3bb148a42d32b498b96202918fca0235d1e4c227
Youbin Jang <ahdtlf2002@sookmyung.ac.kr>
2025-02-04 16:13:55 +0900

    [Add] 키보드 위치에 따라 화면 조정
    react-native-keyboard-aware-scroll-view 라이브러리의 KeyboardAwareScrollView 컴포넌트를 사용해 TextInput 컴포넌트를 클릭하면 키보드가 뜨면서 화면이 살짝 위로 이동함.

--- app/screens/Login.js
+++ app/screens/Login.js
@@ -2,6 +2,7 @@ import React, { useRef, useState } from "react";
 import { Button } from "react-native";
 import styled from "styled-components";
 import { Image, Input } from "../components";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 const Login = ({ navigation }) => {
   const [email, setEmail] = useState("");
@@ -9,31 +10,36 @@ const Login = ({ navigation }) => {
   const passwordRef = useRef(); // 이메일 -> 비밀번호로 포커스 이동
 
   return (
-    <Container>
-      <Image
-        uri={require("../../assets/logo.png")}
-        imageStyle={{ borderRadius: 8 }}
-      />
-      <Input
-        label="Email"
-        value={email}
-        onChangeText={(text) => setEmail(text)}
-        onSubmitEditing={() => passwordRef.current.focus()}
-        placeholder="Email"
-        returnKeyType="next"
-      />
-      <Input
-        label="Password"
-        value={password}
-        ref={passwordRef}
-        onChangeText={(text) => setPassword(text)}
-        onSubmitEditing={() => {}}
-        placeholder="Password"
-        returnKeyType="done"
-        isPassword
-      />
-      <Button title="Signup" onPress={() => navigation.navigate("Signup")} />
-    </Container>
+    <KeyboardAwareScrollView
+      contentContainerStyle={{ flex: 1 }}
+      extraScrollHeight={20}
+    >
+      <Container>
+        <Image
+          uri={require("../../assets/logo.png")}
+          imageStyle={{ borderRadius: 8 }}
+        />
+        <Input
+          label="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          onSubmitEditing={() => passwordRef.current.focus()}
+          placeholder="Email"
+          returnKeyType="next"
+        />
+        <Input
+          label="Password"
+          value={password}
+          ref={passwordRef}
+          onChangeText={(text) => setPassword(text)}
+          onSubmitEditing={() => {}}
+          placeholder="Password"
+          returnKeyType="done"
+          isPassword
+        />
+        <Button title="Signup" onPress={() => navigation.navigate("Signup")} />
+      </Container>
+    </KeyboardAwareScrollView>
   );
 };
 
